0e98f18c1d1518732840c426b56fad0a1e22c163
Owen <owenheintz03@gmail.com>
2025-09-19 15:34:12 -0500

    random

--- random stock.py
+++ random stock.py
@@ -2,7 +2,7 @@ import csv
 import random
 
 def random_stock():
-    with open("nasdaq_screener_1758143846061.csv", mode='r') as file:
+    with open("ticker_list.csv", mode='r') as file:
         reader = csv.reader(file)
         stock_list = list(reader)
     
