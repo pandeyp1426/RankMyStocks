b13ec173fce03cc19b249d88b725e75d8f04688a
Sam Hudson <sam_hudson13@outlook.com>
2025-09-22 15:42:08 -0500

    Working Random stock grabber and API call

--- random stock.py
+++ random stock.py
@@ -1,5 +1,8 @@
 import csv
 import random
+import urllib.request
+import json
+import urllib.parse
 
 def random_stock():
     with open("nasdaq_screener_1758143846061.csv", mode='r') as file:
@@ -9,7 +12,16 @@ def random_stock():
     if not stock_list:
         return None
     
-    return random.choice(stock_list)[0]
+    return random.choice(stock_list)[0] 
 
+function = "TIME_SERIES_DAILY"
 stock = random_stock()
-print(stock)
+
+base = 'https://www.alphavantage.co/query'
+params = {'function': function, 'symbol': stock, 'apikey': '1TOOAS77U9X4GJSC'}
+url = base + '?' + urllib.parse.urlencode(params)
+
+Website_link = urllib.request.urlopen(url)
+wjson = Website_link.read()
+wjdata = json.loads(wjson)
+print(wjdata)
