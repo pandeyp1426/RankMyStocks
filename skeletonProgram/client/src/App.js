e6cbf8af257d6a2087381f2f7c88850fb561838b
Tony Skoglund <antyskoglund@gmail.com>
2025-09-22 16:22:00 -0500

    Added nasdaq Screener to program

--- skeletonProgram/client/src/App.js
+++ skeletonProgram/client/src/App.js
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-
-  const [data, setData] = useState({});
+  const [memberList, setMemberList] = useState({});
   useEffect(() => {
     fetch ("/members").then(
       res=>res.json()
     ).then(
-      data=>{setData(data)
-      console.log(data)
+      data => {
+        setMemberList(data) 
+        console.log(data)
       }
     )
   }, [])
 
   return (
     <div>
-      {(typeof data.members === 'undefined') ? ( //checks if data exists yet from api
+      {(typeof memberList.members === 'undefined') ? ( //checks if data exists yet from api
         <p>Loading...</p> //return Loading... if it doesn't
       ) : (
-        data.members.map((member, i) => (//if returned display them one by one
+        memberList.members.map((member, i) => (//if returned display them one by one
           <p key={i}>{member}</p>
         ))
       )}
