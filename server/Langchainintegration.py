864da2b211721522d56393023593e1f00c822e23
Sam Hudson <sam_hudson13@outlook.com>
2025-09-24 15:29:15 -0500

    Changing Langchainintegration name

--- server/Langchainintegration.py
+++ server/Langchainintegration.py
@@ -8,7 +8,7 @@ from langchain_openai import OpenAI
 os.environ["OPENAI_API_KEY"] = "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"
 
 def random_stock():
-    with open("nasdaq_screener_1758143846061.csv", mode='r') as file:
+    with open("server/nasdaq_screener_1758143846061.csv", mode='r') as file:
         reader = csv.reader(file)
         stock_list = list(reader)
     
