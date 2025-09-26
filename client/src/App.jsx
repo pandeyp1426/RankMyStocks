dda7b5ece15d020f822feda3a4cf8089b53491ab
Tony Skoglund <antyskoglund@gmail.com>
2025-09-26 15:46:36 -0500

    Manually bringing in client changes from 'TonyS' branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Layout } from './layout.jsx'
+import { Home } from './Pages/home.jsx'
+import { MyPortfolios } from './Pages/myPortfolios.jsx'
+import { PortfolioRankings } from './Pages/portfolioRankings.jsx'
+import { Questionair } from './Pages/questionair.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className='text-grey-500'>RankMyStocks</h1>
-      <p class="text">Invest Smarter<span class="blinking">.</span></p>
-    </>
+    <Router>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/myPortfolios" element={<MyPortfolios/>}/>
+          <Route path="/portfolioRankings" element={<PortfolioRankings/>}/>
+          <Route path="/questionair" element={<Questionair/>}/>
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client/src/Pages/home.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+export function Home() {
+    return (
+        <>
+        <div class = 'header'>
+            <h1 className='text-grey-500'>RankMyStocks</h1>
+            <p class="text">Invest Smarter</p>
+            {/* Needs a pop up window after and a function for how the questionair works */}
+            <Link to="/questionair">
+                <button>Create Portfolio</button>
+            </Link>
+        </div>
+        </>
+    )
+}
