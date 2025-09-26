8b84d90fe1367cf56d10510cc21d26fac3b45e3c
Tony Skoglund <antyskoglund@gmail.com>
2025-09-26 12:25:20 -0500

    Committing for home pc

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import './App.css'
+import './Home.css'
 
-function App() { 
+function Home() { 
 
   return (
     <>
+      <div class = 'header'>
       <h1 className='text-grey-500'>RankMyStocks</h1>
       <p class="text">Invest Smarter</p>
       <button>Create Portfolio</button>
+      </div>
       <p>
       <button onClick={() => navigate("client/myPortfolios.jsx")}>Go to My Portfolios</button>
       <button onClick={() => navigate("client/portfolioRankings.jsx")}>Go to Portfolio Rankings</button>
@@ -16,4 +18,4 @@ function App() {
   )
 }
 
-export default App
+export default Home
